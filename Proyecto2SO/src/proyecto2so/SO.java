32d9622924c6b1496d24ec9d2b8adffbba669ff7
Daniel Morillo <daniel.morillo@correo.unimet.edu.ve>
2024-03-03 12:58:20 -0400

    Changes

--- Proyecto2SO/src/proyecto2so/SO.java
+++ Proyecto2SO/src/proyecto2so/SO.java
@@ -4,25 +4,33 @@
  */
 package proyecto2so;
 
+import java.util.concurrent.Semaphore;
+
 /**
  *
  * @author user
  */
-public class SO {
+public class SO  {
     
     private Company Company1;
+    private int Company1CharCounter;
     private Company Company2;
+    private int Company2CharCounter;
     private int countCycle;
+    private Semaphore mutex;
     
     
-    public  SO(Company Company1 , Company Company2){
+    public  SO(Company Company1 , Company Company2, Semaphore mutex){
         
         this.Company1 = Company1;
         this.Company2 = Company2;
         this.countCycle = 0;
+        this.mutex = mutex;
+        this.Company1CharCounter = 0;
+        this.Company2CharCounter = 0;
     }
     
-    public Character[] combatientes(){
+    public Character[] combatientes() throws InterruptedException{
         
         Character[] CharactersArray = new Character[2];
         Character rsCharacter = null;
@@ -30,31 +38,31 @@ public class SO {
         
         if(!Company1.getPriority1().esVacia()){
         
-            rsCharacter = Company1.getPriority1().leerCabeza();
+            rsCharacter = getCompany1().getPriority1().leerCabeza();
         
         }else{
             
             if(!Company1.getPriority2().esVacia()){
                 
-                rsCharacter = Company1.getPriority2().leerCabeza();
-                Company1.getPriority2().desencolar();
-                Company1.getPriority1().encolar(rsCharacter);
+                rsCharacter = getCompany1().getPriority2().leerCabeza();
+                getCompany1().getPriority2().desencolar();
+                getCompany1().getPriority1().encolar(rsCharacter);
             
             }else{
             
                 if(!Company1.getPriority3().esVacia()){
                 
-                rsCharacter = Company1.getPriority3().leerCabeza();
-                Company1.getPriority3().desencolar();
-                Company1.getPriority1().encolar(rsCharacter);
+                rsCharacter = getCompany1().getPriority3().leerCabeza();
+                    getCompany1().getPriority3().desencolar();
+                    getCompany1().getPriority1().encolar(rsCharacter);
                 
                 }else{
                     
                     if(!Company1.getReinforcements().esVacia()){
                 
-                        rsCharacter = Company1.getReinforcements().leerCabeza();
-                        Company1.getReinforcements().desencolar();
-                        Company1.getPriority1().encolar(rsCharacter);
+                        rsCharacter = getCompany1().getReinforcements().leerCabeza();
+                        getCompany1().getReinforcements().desencolar();
+                        getCompany1().getPriority1().encolar(rsCharacter);
                     
                 }else{
                         
@@ -72,31 +80,31 @@ public class SO {
         
         if(!Company2.getPriority1().esVacia()){
         
-            avCharacter = Company2.getPriority1().leerCabeza();
+            avCharacter = getCompany2().getPriority1().leerCabeza();
         
         }else{
             
             if(!Company2.getPriority2().esVacia()){
                 
-                avCharacter = Company2.getPriority2().leerCabeza();
-                Company2.getPriority2().desencolar();
-                Company2.getPriority1().encolar(avCharacter);
+                avCharacter = getCompany2().getPriority2().leerCabeza();
+                getCompany2().getPriority2().desencolar();
+                getCompany2().getPriority1().encolar(avCharacter);
             
             }else{
             
                 if(!Company2.getPriority3().esVacia()){
                 
-                avCharacter = Company2.getPriority3().leerCabeza();
-                Company2.getPriority3().desencolar();
-                Company2.getPriority1().encolar(avCharacter);
+                avCharacter = getCompany2().getPriority3().leerCabeza();
+                    getCompany2().getPriority3().desencolar();
+                    getCompany2().getPriority1().encolar(avCharacter);
                 
                 }else{
                     
                     if(!Company2.getReinforcements().esVacia()){
                 
-                        avCharacter = Company2.getReinforcements().leerCabeza();
-                        Company2.getReinforcements().desencolar();
-                        Company2.getPriority1().encolar(avCharacter);
+                        avCharacter = getCompany2().getReinforcements().leerCabeza();
+                        getCompany2().getReinforcements().desencolar();
+                        getCompany2().getPriority1().encolar(avCharacter);
                     
                 }else{
                         
@@ -113,7 +121,6 @@ public class SO {
         CharactersArray[0] = rsCharacter;
         CharactersArray[1] = avCharacter;
         
-        
         return CharactersArray;
         
     }
@@ -126,31 +133,31 @@ public class SO {
         
         
         if(!Company1.getPriority1().esVacia()){
-            pAux = Company1.getPriority1().getpLast();
+            pAux = getCompany1().getPriority1().getpLast();
             
             while(pAux != null){
                 
                 if(pAux.getElemento().getPriorityCounter() == 8){
                     pAux.getElemento().setPriorityCounter(0);
-                    pAux.getpNext();
+                    pAux = pAux.getpNext();
                 }else{
                     pAux.getElemento().setPriorityCounter(pAux.getElemento().getPriorityCounter() + 1);
-                    pAux.getpNext();
+                    pAux = pAux.getpNext();
                 }
             }
         }
         
         if(!Company2.getPriority1().esVacia()){
-            pAux = Company2.getPriority1().getpLast();
+            pAux = getCompany2().getPriority1().getpLast();
             
             while(pAux != null){
                 
                 if(pAux.getElemento().getPriorityCounter() == 8){
                     pAux.getElemento().setPriorityCounter(0);
-                    pAux.getpNext();
+                    pAux = pAux.getpNext();
                 }else{
                     pAux.getElemento().setPriorityCounter(pAux.getElemento().getPriorityCounter() + 1);
-                    pAux.getpNext();
+                    pAux = pAux.getpNext();
                 }
             }
         }
@@ -158,33 +165,33 @@ public class SO {
         
         
         if(!Company1.getPriority2().esVacia()){
-            pAux = Company1.getPriority2().getpLast();
+            pAux = getCompany1().getPriority2().getpLast();
             
             while(pAux != null){
                 
                 if(pAux.getElemento().getPriorityCounter() == 8){
                     pAux.getElemento().setPriorityCounter(0);
-                    Company1.getPriority1().encolar(pAux.getElemento());
-                    pAux.getpNext();
+                    getCompany1().getPriority1().encolar(pAux.getElemento());
+                    pAux = pAux.getpNext();
                 }else{
                     pAux.getElemento().setPriorityCounter(pAux.getElemento().getPriorityCounter() + 1);
-                    pAux.getpNext();
+                    pAux = pAux.getpNext();
                 }
             }
         }
         
         if(!Company2.getPriority2().esVacia()){
-            pAux = Company2.getPriority2().getpLast();
+            pAux = getCompany2().getPriority2().getpLast();
             
             while(pAux != null){
                 
                 if(pAux.getElemento().getPriorityCounter() == 8){
                     pAux.getElemento().setPriorityCounter(0);
-                    Company2.getPriority1().encolar(pAux.getElemento());
-                    pAux.getpNext();
+                    getCompany2().getPriority1().encolar(pAux.getElemento());
+                    pAux = pAux.getpNext();
                 }else{
                     pAux.getElemento().setPriorityCounter(pAux.getElemento().getPriorityCounter() + 1);
-                    pAux.getpNext();
+                    pAux = pAux.getpNext();
                 }
             }
         }
@@ -194,38 +201,131 @@ public class SO {
         
         
         if(!Company1.getPriority3().esVacia()){
-            pAux = Company1.getPriority3().getpLast();
+            pAux = getCompany1().getPriority3().getpLast();
             
             while(pAux != null){
                 
                 if(pAux.getElemento().getPriorityCounter() == 8){
                     pAux.getElemento().setPriorityCounter(0);
-                    Company1.getPriority2().encolar(pAux.getElemento());
-                    pAux.getpNext();
+                    getCompany1().getPriority2().encolar(pAux.getElemento());
+                    pAux = pAux.getpNext();
                 }else{
                     pAux.getElemento().setPriorityCounter(pAux.getElemento().getPriorityCounter() + 1);
-                    pAux.getpNext();
+                    pAux= pAux.getpNext();
                 }
             }
         }
         
         if(!Company2.getPriority3().esVacia()){
-            pAux = Company2.getPriority3().getpLast();
+            pAux = getCompany2().getPriority3().getpLast();
             
             while(pAux != null){
                 
                 if(pAux.getElemento().getPriorityCounter() == 8){
                     pAux.getElemento().setPriorityCounter(0);
-                    Company2.getPriority2().encolar(pAux.getElemento());
-                    pAux.getpNext();
+                    getCompany2().getPriority2().encolar(pAux.getElemento());
+                    pAux= pAux.getpNext();
                 }else{
                     pAux.getElemento().setPriorityCounter(pAux.getElemento().getPriorityCounter() + 1);
-                    pAux.getpNext();
+                    pAux= pAux.getpNext();
                 }
             }
         }
         
-        this.countCycle =+ 1;
+        this.setCountCycle(+ 1);
         
+        if (getCountCycle() >= 2){
+            
+            if (Math.random() >= 0.2) {
+                
+                getCompany1().CreateCharacter();
+                getCompany2().CreateCharacter();
+            }
+        }
+        
+    }
+
+    /**
+     * @return the Company1
+     */
+    public Company getCompany1() {
+        return Company1;
+    }
+
+    /**
+     * @param Company1 the Company1 to set
+     */
+    public void setCompany1(Company Company1) {
+        this.Company1 = Company1;
+    }
+
+    /**
+     * @return the Company2
+     */
+    public Company getCompany2() {
+        return Company2;
+    }
+
+    /**
+     * @param Company2 the Company2 to set
+     */
+    public void setCompany2(Company Company2) {
+        this.Company2 = Company2;
+    }
+
+    /**
+     * @return the countCycle
+     */
+    public int getCountCycle() {
+        return countCycle;
+    }
+
+    /**
+     * @param countCycle the countCycle to set
+     */
+    public void setCountCycle(int countCycle) {
+        this.countCycle = countCycle;
+    }
+
+    /**
+     * @return the mutex
+     */
+    public Semaphore getMutex() {
+        return mutex;
+    }
+
+    /**
+     * @param mutex the mutex to set
+     */
+    public void setMutex(Semaphore mutex) {
+        this.mutex = mutex;
+    }
+
+    /**
+     * @return the Company1CharCounter
+     */
+    public int getCompany1CharCounter() {
+        return Company1CharCounter;
+    }
+
+    /**
+     * @param Company1CharCounter the Company1CharCounter to set
+     */
+    public void setCompany1CharCounter(int Company1CharCounter) {
+        this.Company1CharCounter = Company1CharCounter;
+    }
+
+    /**
+     * @return the Company2CharCounter
+     */
+    public int getCompany2CharCounter() {
+        return Company2CharCounter;
+    }
+
+    /**
+     * @param Company2CharCounter the Company2CharCounter to set
+     */
+    public void setCompany2CharCounter(int Company2CharCounter) {
+        this.Company2CharCounter = Company2CharCounter;
     }
 }
--- Proyecto2SO/src/proyecto2so/Simulacion.java
+++ Proyecto2SO/src/proyecto2so/Simulacion.java
@@ -4,6 +4,10 @@
  */
 package proyecto2so;
 
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author user
@@ -14,6 +18,7 @@ public class Simulacion extends Thread{
     private Company company2;
     private SO so;
     private AI ai;
+    private Semaphore mutex;
     
     
     public Simulacion(Company company1, Company company2, SO so, AI ai){
@@ -22,15 +27,97 @@ public class Simulacion extends Thread{
         this.company2 = company2;
         this.so = so;
         this.ai =  ai;
+        this.mutex = new Semaphore(1);
     }
     
     @Override
     public void run(){
-        for (int i = 0; i < 13; i++) { //When the simulation is started, it generates the first 20 characters
-            company1.CreateCharacter();
-            company2.CreateCharacter();
-            
-
+        //Creacion de personajes
+        for (int i = 0; i < 12; i++) { //When the simulation is started, it generates the first 20 characters
+            getCompany1().CreateCharacter();
+            getCompany2().CreateCharacter();
         }
+        
+        
+        while (true) {
+            try {
+                //Escogencia de primeros luchadores
+                Character[] Brawlers= getSo().combatientes();
+                System.out.println("Combatientes escogidos");
+                getAi().starBattle(Brawlers[0], Brawlers[1]);
+                
+                getSo().actualizarColas();
+                System.out.println("Colas actualizadas");
+                
+                System.out.println("\n------------------------------------------\n");
+                
+                
+                
+                
+                
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Simulacion.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+            
+        
+        
+    }
+
+    /**
+     * @return the company1
+     */
+    public Company getCompany1() {
+        return company1;
+    }
+
+    /**
+     * @param company1 the company1 to set
+     */
+    public void setCompany1(Company company1) {
+        this.company1 = company1;
+    }
+
+    /**
+     * @return the company2
+     */
+    public Company getCompany2() {
+        return company2;
+    }
+
+    /**
+     * @param company2 the company2 to set
+     */
+    public void setCompany2(Company company2) {
+        this.company2 = company2;
+    }
+
+    /**
+     * @return the so
+     */
+    public SO getSo() {
+        return so;
+    }
+
+    /**
+     * @param so the so to set
+     */
+    public void setSo(SO so) {
+        this.so = so;
+    }
+
+    /**
+     * @return the ai
+     */
+    public AI getAi() {
+        return ai;
+    }
+
+    /**
+     * @param ai the ai to set
+     */
+    public void setAi(AI ai) {
+        this.ai = ai;
     }
 }
